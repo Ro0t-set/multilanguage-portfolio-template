f44fb1a4af0802a323d322e39dec972b1a872674
Ro0t-set <tommaso.patriti@studio.unibo.it>
2025-02-23 18:06:21 +0100

    refactor(vite.config.ts): simplify defineConfig function by removing unnecessary mode parameter and conditional base setting

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "./" : "/",
+export default defineConfig({
+  base:"./" ,
   plugins: [
     vue({
       template: { transformAssetUrls }
@@ -16,4 +16,4 @@ export default defineConfig(({ mode }) => ({
       )
     })
   ],
-}))
+})
