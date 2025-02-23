639977c6f659f8bc8ac75de1d0372635fb564be6
Ro0t-set <tommaso.patriti@studio.unibo.it>
2025-02-23 22:29:18 +0100

    feat(NavBar.vue): add responsive design for mobile devices by adjusting tab alignment and layout style(NavBar.vue): update tab styling for better visual consistency and user experience style(style.css): remove unnecessary styles and improve readability of code

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,6 +2,9 @@
 import { computed } from 'vue'
 import { Language } from '../locale/locale.d'
 import { useAppStore } from '../stores/app'
+import { useQuasar } from 'quasar'
+
+const $q = useQuasar()
 const store = useAppStore()
 
 const toggleButtonLabel = computed(() =>
@@ -9,54 +12,72 @@ const toggleButtonLabel = computed(() =>
 )
 
 const emit = defineEmits(['scrollToSection'])
-
 function onTabClick(tabName: string) {
   emit('scrollToSection', tabName)
+}
+
+const isMobile = computed(() => $q.screen.lt.sm)
+
+function getLabel(tabKey: keyof typeof store.currentDictionary.navbar) {
+  return isMobile.value ? '' : store.currentDictionary.navbar[tabKey]
 }
 </script>
 
 <template>
-  <q-toolbar class="fixed-top bg-primary shadow-1 rounded-borders">
-    <q-space />
-    <q-tabs
-      shrink
-      stretch
-    >
-      <q-tab
-        name="about"
-        :label="store.currentDictionary.navbar.about"
-        @click="onTabClick('about')"
-      />
-      <q-tab
-        name="experience"
-        :label="store.currentDictionary.navbar.experience"
-        @click="onTabClick('experience')"
-      />
-      <q-tab
-        name="skills"
-        :label="store.currentDictionary.navbar.skills"
-        @click="onTabClick('skills')"
-      />
-      <q-tab
-        name="projects"
-        :label="store.currentDictionary.navbar.projects"
-        @click="onTabClick('projects')"
-      />
-      <q-tab
-        name="contact"
-        :label="store.currentDictionary.navbar.contact"
-        @click="onTabClick('contact')"
-      />
-    </q-tabs>
-    <q-btn
-      :label="toggleButtonLabel"
-      @click="store.toggleLanguage"
+  <q-space v-if="!isMobile" />
+  <q-tabs
+    shrink
+    stretch
+    :align="isMobile ? 'justify' : 'right'"
+    :spread="isMobile"
+    :class="[isMobile ? 'fixed-bottom' : 'fixed-top', 'bg-primary', 'shadow-1']"
+  >
+    <q-tab
+      name="about"
+      icon="person"
+      :label="getLabel('about')"
+      @click="onTabClick('about')"
     />
-  </q-toolbar>
+    <q-tab
+      name="experience"
+      icon="work"
+      :label="getLabel('experience')"
+      @click="onTabClick('experience')"
+    />
+    <q-tab
+      name="skills"
+      icon="code"
+      :label="getLabel('skills')"
+      @click="onTabClick('skills')"
+    />
+    <q-tab
+      name="projects"
+      icon="folder"
+      :label="getLabel('projects')"
+      @click="onTabClick('projects')"
+    />
+    <q-tab
+      name="contact"
+      icon="mail"
+      :label="getLabel('contact')"
+      @click="onTabClick('contact')"
+    />
+  <q-btn
+    flat
+    icon="flag_circle"
+    :label="toggleButtonLabel"
+    @click="store.toggleLanguage"
+  />
+</q-tabs>
 </template>
 
 <style scoped>
-.q-toolbar {
+.q-tabs {
   z-index: 2000;
 }
+
+.q-tab {
+  min-height: 48px;
+}
+
 </style>
